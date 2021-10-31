fcb6c3495633f6f4c07cc4e86ea75dd7796020ef
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-31 16:09:52 +0600

    added masonry css

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,7 +1,8 @@
-import { Alert, Container, Grid, Snackbar } from "@mui/material";
+import { Alert, Container, Snackbar } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import NoteCard from "../components/NoteCard";
+import Masonry from "react-masonry-css";
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -35,15 +36,25 @@ export default function Notes() {
     setOpenSnackBar(false);
   };
 
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  };
+
   return (
     <Container>
-      <Grid container spacing={3}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {notes.map((note) => (
-          <Grid item xs={12} md={6} lg={4} key={note._id}>
+          <div key={note._id}>
             <NoteCard note={note} handleDelete={handleDelete} />
-          </Grid>
+          </div>
         ))}
-      </Grid>
+      </Masonry>
       <Snackbar
         open={openSnackbar}
         autoHideDuration={6000}

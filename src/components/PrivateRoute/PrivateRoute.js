fc7114a3fdf61fbc9368c674da0c8787260b168d
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-27 16:34:08 +0600

    refactored private route

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,18 +1,22 @@
+// Import necessary modules
 import { CircularProgress } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import { Redirect, Route } from "react-router";
 import useAuth from "../../hooks/useAuth";
 
+// Main Private Route
 const PrivateRoute = ({ children, ...rest }) => {
   const { user, isLoading } = useAuth();
   if (isLoading) {
+    // Return if the window is reloading
     return (
       <Box sx={{ position: "absolute", left: "50%", top: "50%" }}>
         <CircularProgress />
       </Box>
     );
   }
+  // Render for general time
   return (
     <Route
       {...rest}
